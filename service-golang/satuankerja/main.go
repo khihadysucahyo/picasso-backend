37e5386352fbf2d4ef53deb48d4cf21cfa84b5d2
arifboyz16 <arifboyz16@gmail.com>
2020-02-09 16:02:01 +0700

    rebuild service golang

--- service-golang/satuankerja/main.go
+++ service-golang/satuankerja/main.go
@@ -1,23 +1,5 @@
 package main
-
-import (
-	"fmt"
-	"database/sql"
-	"log"
-	"os"
-	"net/http"
-	"github.com/joho/godotenv"
-)
-
+import "fmt"
 func main() {
-	err := godotenv.Load("../../.env")
-  if err != nil {
-    log.Fatal("Error loading .env file")
-		godotenv.Load(".env")
-  }
-
-  s3Bucket := os.Getenv("POSTGRESQL_HOST")
-	fmt.Println(s3Bucket)
-	// http.HandleFunc("/satker/create", handleCreate)
-	log.Fatal(http.ListenAndServe(":8301", nil))
+    fmt.Println("hello world")
 }
