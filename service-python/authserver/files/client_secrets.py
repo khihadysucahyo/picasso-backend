f09e2c54b57fc0c58bb16a62084efb12bb1c72d8
arifboyz16 <arifboyz16@gmail.com>
2020-03-09 01:14:31 +0700

    client_secrets

--- service-python/authserver/files/client_secrets.py
+++ service-python/authserver/files/client_secrets.py
@@ -1,6 +1,6 @@
 { "web":
-  { "client_id": "724643044802-fg4v36kp8a8rmro8hoj3in03anfdspb9.apps.googleusercontent.com",
-    "client_secret": "Cu5twZpD2MlPrPKSTo2gZqZN",
+  { "client_id": "724643044802-qm0h2ho4fj0ahoc99ejjqcn5usf1lmlv.apps.googleusercontent.com",
+    "client_secret": "pXIsil_FxbP1DPDVsUohMjL2",
     "redirect_uris": [ "postmessage"],
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://accounts.google.com/o/oauth2/token"
