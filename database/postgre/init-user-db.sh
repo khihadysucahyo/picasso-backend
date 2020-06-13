3be35338c766a13e763d52bd82e2ac336470872a
Arif <arifboyz16@gmail.com>
2020-06-13 21:40:47 +0700

    docker env configuration

--- database/postgre/init-user-db.sh
+++ database/postgre/init-user-db.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 create_user() {
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRESQL_USER" <<-EOSQL
     CREATE USER adminpostgre WITH PASSWORD 'plokijuh';
     ALTER ROLE adminpostgre SET client_encoding TO 'utf8';
     ALTER ROLE adminpostgre SET default_transaction_isolation TO 'read committed';
@@ -16,7 +16,7 @@ EOSQL
 create_database() {
 	local database=$1
 	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRESQL_USER" <<-EOSQL
 	    CREATE DATABASE $database;
 	    GRANT ALL PRIVILEGES ON DATABASE $database TO "adminpostgre";
 EOSQL
