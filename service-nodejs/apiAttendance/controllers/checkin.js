28307773e68ed357fc90ced7b1457d07a2c11570
Arif <arifboyz16@gmail.com>
2020-08-13 10:02:38 +0700

    revamp attendance

--- service-nodejs/apiAttendance/controllers/checkin.js
+++ service-nodejs/apiAttendance/controllers/checkin.js
@@ -51,7 +51,7 @@ module.exports = async (req, res) => { // eslint-disable-line
         const rules = [{
             $match: {
                 'createdBy.email': session.email,
-                createdAt: {
+                startDate: {
                     $gte: new Date(start),
                     $lt: new Date(end)
                 }
